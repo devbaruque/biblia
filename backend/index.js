fef6a472f5199b1b22cdd7ce6f41e320644cb1ab
Luiz Rocha <dev.baruque@gmail.com>
2025-03-08 16:48:07 -0300

    Correção dos métodos de entrada de dados para que o usuário possa digitar os valores de entrada.

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,34 @@
 const express = require("express");
-const cors = require("cors"); // Importe o pacote cors
+const cors = require("cors");
 const app = express();
 
 // Configura o CORS para permitir requisições do frontend
 app.use(
   cors({
     origin: "https://santosaber.vercel.app", // URL do frontend
-    methods: ["GET", "POST"], // Métodos permitidos
+    methods: ["GET", "POST", "OPTIONS"], // Métodos permitidos
     allowedHeaders: ["Content-Type"], // Cabeçalhos permitidos
   })
 );
 
 app.use(express.json());
 
-// Rota /api/chat
+// Rota OPTIONS para /api/chat
+app.options("/api/chat", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://santosaber.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.send();
+});
+
+// Rota POST para /api/chat
 app.post("/api/chat", async (req, res) => {
   const { message } = req.body;
 
   console.log("Pergunta recebida:", message); // Log da pergunta
 
   try {
-    const response = await fetch("https://biblia-puug.vercel.app/api/chat", {
+    const response = await fetch("https://chatbiblia.fly.dev/api/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
